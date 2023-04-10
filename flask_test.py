a42a93613a6982291fc532d54dd1ac1b2fd55bed
LEXX <lexx_well@mail.ru>
2023-04-10 11:36:27 +0300

    добавил отображение второй странички надписи на страницы оформил тегами заголовка html

--- flask_test.py
+++ flask_test.py
@@ -5,7 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'index'
+    return '<h1>основная страничка</h1>'
+
+
+@app.route('/about')
+def about():
+    return '<h2>о сайте</h2>'
 
 
 if __name__ == '__main__':

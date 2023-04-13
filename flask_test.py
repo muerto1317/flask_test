5b6985149b8f80c6b0f3aab14b4e2beec2ba2d0f
LEXX <lexx_well@mail.ru>
2023-04-13 15:01:29 +0300

    начинаем создавать шаблон в html вставил тэг в заголовке в коде создал его чтение

--- flask_test.py
+++ flask_test.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title='Главная страница')
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='ABOUT')
 
 
 if __name__ == '__main__':

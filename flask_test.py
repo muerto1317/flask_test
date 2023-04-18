01af615f0d704eb0fa0d960a19c128b620e03953
LEXX <lexx_well@mail.ru>
2023-04-18 12:07:02 +0300

    функция url_for снизу в консоле видим адрес запрашиваемой страницы

--- flask_test.py
+++ flask_test.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -7,11 +7,13 @@ menu = ["установка", "первое приложение", "обратн
 
 @app.route('/')
 def index():
+    print(url_for('index'))
     return render_template('index.html', menu=menu)
 
 
 @app.route('/about')
 def about():
+    print(url_for('about'))
     return render_template('about.html', title='ABOUT', menu=menu)
 
 
